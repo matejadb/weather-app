19b416d49f46d4b733fb6c3ce5d35a0b47f3eddd
matejadb <92729211+matejadb@users.noreply.github.com>
2025-11-11 13:16:59 +0100

    add working drorpdown checked state with saved units

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,77 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-	document.getElementById('myDropdown').classList.toggle('show');
+const global = {
+	units: {
+		temperature: 'celsius',
+		windSpeed: 'km/h',
+		precipitation: 'millimeters',
+	},
+};
+
+const dropdownTemperature = document.querySelector('.temperature');
+const dropdownWindSpeed = document.querySelector('.wind-speed');
+const dropdownPrecipitation = document.querySelector('.precipitation');
+const dropdownBtn = document.querySelector('.btn-dropdown');
+const dropdownContent = document.querySelector('.units-dropdown-content');
+
+function changeTemperatureUnits(e) {
+	if (e.target.classList.contains('units-dropdown-item')) {
+		dropdownTemperature.querySelectorAll('a').forEach((unit) => {
+			unit.classList.remove('checked');
+			unit.children[1].classList.add('hidden');
+		});
+		e.target.classList.add('checked');
+		e.target.children[1].classList.remove('hidden');
+		global.units.temperature = e.target.textContent
+			.trim()
+			.split(' ')[0]
+			.toLowerCase();
+	}
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-	if (!event.target.matches('.item')) {
-		var dropdowns = document.getElementsByClassName('units-dropdown-content');
-		var i;
-		for (i = 0; i < dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
+function changeWindSpeedUnits(e) {
+	if (e.target.classList.contains('units-dropdown-item')) {
+		dropdownWindSpeed.querySelectorAll('a').forEach((unit) => {
+			unit.classList.remove('checked');
+			unit.children[1].classList.add('hidden');
+		});
+
+		e.target.classList.add('checked');
+		e.target.children[1].classList.remove('hidden');
+		global.units.windSpeed = e.target.textContent
+			.trim()
+			.split(' ')[0]
+			.toLowerCase();
 	}
-};
+}
+
+function changePrecipitationUnits(e) {
+	if (e.target.classList.contains('units-dropdown-item')) {
+		dropdownPrecipitation.querySelectorAll('a').forEach((unit) => {
+			unit.classList.remove('checked');
+			unit.children[1].classList.add('hidden');
+		});
+
+		e.target.classList.add('checked');
+		e.target.children[1].classList.remove('hidden');
+		global.units.precipitation = e.target.textContent
+			.trim()
+			.split(' ')[0]
+			.toLowerCase();
+	}
+}
+
+function openUnitsDropdown(e) {
+	e.stopPropagation();
+	dropdownContent.classList.toggle('show');
+}
+
+function closeUnitsDropdown(e) {
+	if (!dropdownContent.contains(e.target) && e.target !== dropdownBtn) {
+		dropdownContent.classList.remove('show');
+	}
+}
+
+dropdownTemperature.addEventListener('click', changeTemperatureUnits);
+dropdownWindSpeed.addEventListener('click', changeWindSpeedUnits);
+dropdownPrecipitation.addEventListener('click', changePrecipitationUnits);
+dropdownBtn.addEventListener('click', openUnitsDropdown);
+window.addEventListener('click', closeUnitsDropdown);
